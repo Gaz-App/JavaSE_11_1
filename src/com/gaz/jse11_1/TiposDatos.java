5ebead16519e72b3f54d5e1b8d539f4df0b7fc75
GazApp <juangaz@hotmail.com>
2021-11-17 16:58:27 -0600

    Initial commit

---
+++ src/com/gaz/jse11_1/TiposDatos.java
@@ -0,0 +1,11 @@
+package com.gaz.jse11_1;
+
+public class TiposDatos{
+
+    public static void main(String[] args) {
+        float flotante= 44444.44f;
+        double doble = 3333333333.00D;
+
+        System.out.println(doble);
+    }
+}
